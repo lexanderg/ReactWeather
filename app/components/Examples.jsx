a4d1aa1da9f436d8c339f9f976e9cf3f11a893ab
Alexander Gomez <alexander@Alexanders-iMac.local>
2017-04-19 14:38:31 +0200

    Added copy

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,6 +1,12 @@
 var React = require('react');
 
 
-var Examples = () => <h3>Examples Component!!!</h3>;
-
+var Examples = () => {
+    return (
+    <div>
+      <h3>Examples Component!!!</h3>
+      <p>Welcome to the examples page!</p>
+    </div>
+    )
+}
 module.exports = Examples;
