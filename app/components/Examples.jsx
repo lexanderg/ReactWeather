c5794d7f971b68963cc15c23ba409bfba5682466
Alexander Gomez <alexander@Alexanders-iMac.local>
2017-04-20 08:42:55 +0200

    Add gird system & styles examples

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,11 +1,19 @@
 var React = require('react');
-
+var {Link} = require('react-router');
 
 var Examples = () => {
     return (
     <div>
-      <h3>Examples</h3>
-      <p>Welcome to the examples page!</p>
+      <h3 className="text-center">Examples</h3>
+      <p>Here a few example locations to try out:</p>
+      <ol>
+        <li>
+          <Link to='/?location=Amsterdam'>Amsterdam, NL</Link>
+        </li>
+        <li>
+          <Link to='/?location=London'>London, UK</Link>
+        </li>
+      </ol>
     </div>
     )
 }
